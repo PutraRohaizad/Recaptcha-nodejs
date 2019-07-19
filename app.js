cb92badbb2c333b6b7a2102f1da2ce2264cbd6ca
PutraRohaizad <putrarohayzad@gmail.com>
2019-07-19 09:51:19 +0800

    add captcha

--- app.js
+++ app.js
@@ -33,4 +33,37 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+
+app.post('/login', (req,res) =>{
+
+if(
+  req.body.captcha === undefined ||
+  req.body.captcha === '' ||
+  req.body.captcha === null 
+){
+  return res.json({"success": false, "msg": "Please select captcha"});
+}
+
+// secret key
+  const secretKey = '6LdyRa4UAAAAALqrXZTIrozGPrZ8fG81_4bNJ_Cr';
+
+// verifi url
+  const verifyUrl = `https://google.com/recaptcha/api/siteverify?secret=${secretKey}&response=${req.body.captcha}
+  &remoteip=${req.connection.remoteAddress}`;
+
+// make request to verify url
+request(verifyUrl, (err,response,body) => {
+  body = JSON.parse(body);
+
+  // if not successful
+if(body.success !== undefined && !body.success){
+  return res.json({"success": false, "msg": "Failed captcha verification"});
+}
+  // if success
+  return res.json({"success": true, "msg": " Captcha passed"});
+
+ });
+});
+
+
 module.exports = app;
